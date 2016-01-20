16570bd440f0ba8b760c5e142277171581528f5e
Jude Chavez <Chavezjude7@gmail.com>
2016-01-19 18:02:30 -0800

    added html

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+			milestone-1
+		</title>
+	</head>
+	<h1>Personal Website Milestone-1</h1>
+	<h2>Purpose</h2>
+	<body>
+		The purpose of my PWP is to aid me in finding clients and possible employers who can utilize my services.
+	</body>
+	<h2>Audience</h2>
+	<body>
+		The Audience of my PWP will be potential clients and employers and will also be seen by my peers.
+	</body>
+	<h2>Goal</h2>
+	<body>
+		The goal of my PWP is to show potential clients and employers what services I will be able to provide.
+	</body>
+	<h2>
+		Persona
+	</h2>
+	<body>
+		<p>Name: Bill<br>
+			<br>Bill is a 25 year old small business owner who wishes to expand his customer base and wishes to provide<br>
+			a comprehensive way to get information of the serives his businesses can provide to potential customers.<br>
+			Bill wishes to appeal to his customer base who largely consists of younge people like him. Bill expects<br>
+			his site to be viewed on mobile devices and desktop computers and wishes his site to be able to work with<br>
+			both. Bill runs a small business selling t-shirts so he wants a site that will be able to have pictures<br>
+			and prices for his merchandise.<br>
+			<br>Goals:<br>
+			<br>Bill wishes to expand his customer base and wishes to be able to provide information about his services.<br>
+			<br>Frustrations:<br>
+			<br>Bill is frustrated with not being able to reach his customers efficiently and wishes to have a simpler way<br>
+			of doing it<br>
+			<br>Additional Information<br>
+			Bill has a basic understanding of computers and how to use them. Bill is an apple user and has a Mac, Ipad, and<br>
+			Iphone that are all synced to apples cloud storage service "icloud".<br></P>
+	</body>
+	<h2>Use Case</h2>
+	<body>
+		Bill will update and repond to customers and will make sure they have proper information about what promotions and<br>
+		and deals he will be offering from his business. Bill will be using basic computing power a tablet computer and<br>
+		mobile phone.
+	</body>
+≤
