bff11ed3ab621bc5b0ddcef95ced12f3b045c81e
Jude Chavez <Chavezjude7@gmail.com>
2016-01-27 20:08:54 -0800

    added html

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<title>Milestone-2</title>
+	</head>
+<h1>PWP Milestone-1</h1>
+	<h2>Content Strategy</h2>
+	<body>
+		<p>The Plan I intend to use for my site is to have all of mt content spread between three different pages withing the site. The site will consiste of one main page. Followed by three subpages.
+		<ul>
+			<li>Page 1 - Web Services: A basic description of the web services I can provide such as, a professional website.</li>
+			<li>Page 2 - Music Services: A basic descriptions of the musical services I provide such as, music lessons, and DJ/MC services.</li>
+			<li>Page 3 - About Jude/contact info: List of my contact info like email and professional social media presences.</li>
+		</ul>
+		This page will allow the end user to quickly navigate and find information about me and about the servies I provide along with contact information so they can use my services.</p>
+	</body>
+
+</html>
