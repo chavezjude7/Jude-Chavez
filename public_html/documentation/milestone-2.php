1c9cde5055f1de485984f50c5ac63dd598acfea8
Jude Chavez <Chavezjude7@gmail.com>
2016-01-28 08:43:49 -0800

    fixed images hopefully

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -3,7 +3,7 @@
 	<head>
 		<title>Milestone-2</title>
 	</head>
-<h1>PWP Milestone-1</h1>
+<h1>PWP Milestone-2</h1>
 	<h2>Content Strategy</h2>
 	<body>
 		<p>The Plan I intend to use for my site is to have all of mt content spread between three different pages withing the site. The site will consiste of one main page. Followed by three subpages.
@@ -12,7 +12,7 @@
 			<li>Page 2 - Music Services: A basic descriptions of the musical services I provide such as, music lessons, and DJ/MC services.</li>
 			<li>Page 3 - About Jude/contact info: List of my contact info like email and professional social media presences.</li>
 		</ul>
-		<img src="images/wireframes.svg" alt="not found" >
+		<img src="/images/wireframes.svg" alt="not found" >
 
 		This page will allow the end user to quickly navigate and find information about me and about the servies I provide along with contact information so they can use my services.</p>
 	</body>
@@ -20,6 +20,6 @@
 	<body>
 		<p>The content strategy of this site is to provide users with a comprehensive showcase of the various services I can provide. The first page will show the web services I provide such as website professional design. The second page will provide the user with information on what type of musical services I provide such as DJ/MC and lessons. The third page will show my contact information so that a user may find the information they need to contact me.</p>
 	</body>
-	<img src="images/interactionflow.svg" alt="not found" >
-	<img src="images/sitemap.svg" alt="not found" >
+	<img src="/images/interactionflow.svg" alt="not found" >
+	<img src="/images/sitemap.svg" alt="not found">
 </html>
