4c20526fc5c22fdff86c445f1bb92f35e393c1b8
Jude Chavez <Chavezjude7@gmail.com>
2016-02-23 15:51:01 -0700

    added navbar, photo, and fixed footer issues

---
+++ public_html/pages/mainpage.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<script data-require="jquery@*" data-semver="2.2.0" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+		<script data-require="bootstrap@*" data-semver="3.3.6" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<link data-require="bootstrap-css@3.3.6" data-semver="3.3.6" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.css" />
+		<link href="style.css" rel="stylesheet" />
+		<script src="script.js"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+	</head>
+
+	<body id="content">
+		<div id="header">
+			<h1>Jude Chavez</h1>
+		</div>
+		<div id="content2">
+			<div>
+				<h4>Hello!</h4>
+			</div>
+			<p>
+				Leggings hella synth, jean shorts pork belly seitan offal actually viral roof party vegan. Wolf ugh man bun bushwick. Retro stumptown 90's, kinfolk fashion axe viral bicycle rights food truck sustainable crucifix wayfarers XOXO. Pitchfork roof party kickstarter helvetica paleo butcher, pinterest affogato freegan. Freegan keytar ugh cornhole, before they sold out pabst actually ramps fap tofu. Knausgaard mustache lumbersexual, kickstarter portland fingerstache polaroid 3 wolf moon tattooed kombucha health goth post-ironic occupy pabst. Waistcoat williamsburg +1, quinoa bespoke cray ugh.
+
+				Cray franzen schlitz, quinoa vegan craft beer 3 wolf moon farm-to-table four loko kale chips kickstarter. Scenester biodiesel butcher offal, wayfarers heirloom quinoa. Occupy bitters paleo direct trade man braid skateboard. Venmo yr umami, green juice tote bag dreamcatcher keffiyeh mlkshk YOLO tousled banh mi fanny pack. Slow-carb migas letterpress retro bicycle rights, umami post-ironic. Fixie banh mi tacos, direct trade bicycle rights humblebrag narwhal brooklyn taxidermy street art. Pork belly quinoa dreamcatcher, raw denim paleo trust fund scenester neutra ennui deep v.
+
+				Squid pickled mumblecore, man braid pinterest shabby chic typewriter you probably haven't heard of them craft beer polaroid kogi chambray. Knausgaard PBR&B asymmetrical, distillery lo-fi roof party tote bag pabst stumptown slow-carb hashtag yuccie. Banh mi crucifix twee, shabby chic sustainable man braid literally keffiyeh trust fund migas direct trade locavore church-key squid wolf. Blue bottle pop-up celiac tumblr, flannel meggings bitters single-origin coffee. Typewriter vinyl seitan, fanny pack tacos migas pork belly tattooed selvage kogi venmo salvia. Blog readymade tousled fashion axe tofu. Migas banh mi meggings, letterpress keytar authentic swag actually seitan paleo.
+
+				Aesthetic gochujang helvetica tilde, kogi messenger bag you probably haven't heard of them direct trade occupy typewriter ethical church-key pork belly. Organic brooklyn yr chia gastropub truffaut. Echo park small batch 3 wolf moon deep v, craft beer forage intelligentsia synth listicle readymade messenger bag vegan chartreuse narwhal. Occupy before they sold out chia cardigan viral ugh cray, normcore heirloom mumblecore thundercats biodiesel. Tousled stumptown before they sold out, church-key gastropub farm-to-table tilde retro bushwick next level affogato fap VHS single-origin coffee occupy. Wolf fixie scenester, vinyl franzen deep v vegan quinoa hella crucifix synth. Stumptown four dollar toast VHS raw denim man braid freegan.
+			</p>
+		</div>
+		<div id="footer">
+			<div class="row">
+				<div class="col-md-6">sweg</div>
+				<div class="col-md-6">yolo</div>
+			</div>
+		</div>
+	</body>
+
+</html>
---
+++ public_html/php/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html  lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		<title>Jude-Chavez</title>
+
+		<link rel="stylesheet" type="text/css" href="style.css" />
+		<style type="text/css">
+			#wrapper { height:100%; }
+		</style>
+		<![endif]-->
+
+	</head>
+
+	<body>
+
+		<div id="wrapper">
+
+			<div id="header">
+				<div id="head1">
+					<link href='https://fonts.googleapis.com/css?family=Press+Start+2P' rel='stylesheet' type='text/css'>
+					<h1>Jude Chavez</h1>
+					<h3>Judeicus Maximus</h3>
+				</div>
+			</div><!-- #header -->
+
+			<nav class="navbar-navbar-default">
+				<div class="container-fluid">
+					<div class="navbar-header">
+						<a class="navbar-brand" href="#">Links!</a>
+					</div>
+					<ul class="nav navbar-nav">
+						<li><a href="https://twitter.com/JudeicusMaximus">Twitter</a></li>
+						<li><a href="https://www.facebook.com/judeicusMaximus">Facebook</a></li>
+						<li><a href="https://www.linkedin.com/in/jude-chavez-87a519114">Linked In</a></li>
+						<li><a href="https://soundcloud.com/judeicusmaximus">Soundcloud</a></li>
+						<li><a href="https://www.youtube.com/channel/UCBuoSq2gvaHA5IYaL2FVZMA">YouTube</a></li>
+					</ul>
+				</div>
+			</nav>
+
+			<div id="content">
+				<div id="content1">
+					<p>Albuquerque based web developer who is here for you!</p>
+				</div>
+				<img id="photo" src="images/judespeedo.jpg" alt="a nice picture of me" >
+				<div id="content2">
+					Hello! My name is jude I am a local web developer located in albuquerque New Mexico who is here to assist you in your web design needs!
+
+				</div>
+				<div id="content3"> <!-- content3 -->
+					<h3>Services Provided</h3>
+					<ul class="list">
+						<li>Web development</li>
+						<li>Styling</li>
+						<li>Businesses sites</li>
+						<li>Personal sites</li>
+						<li>Professional sites</li>
+					</ul>
+				</div>
+			</div><!-- #content -->
+			<div id="footer">
+				<div id="footer-content">
+					<p>
+						Phone: (505) 600-9407 or email: Chavezjude7@gmail.com
+					</p>
+				</div>
+			</div><!-- #footer -->
+
+		</div><!-- #wrapper -->
+
+	</body>
+
+</html>
