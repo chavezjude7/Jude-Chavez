7313c9014e175e7da2ef4f1415a40c44d94043d0
Jude Chavez <Chavezjude7@gmail.com>
2016-03-03 18:35:51 -0700

    added icons and form

--- public_html/index.php
+++ public_html/index.php
@@ -5,6 +5,7 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 		<title>Jude-Chavez</title>
 
 		<link rel="stylesheet" type="text/css" href="css/style.css" />
@@ -28,27 +29,61 @@
 
 			</div>
 			<div class="dirt">
-				<h2>About Me</h2>
-				<h3>I'm a webdeveloping nurse</h3>
-				<p id="maincontent">
-					<link href='https://fonts.googleapis.com/css?family=VT323' rel='stylesheet' type='text/css'>
-					You have to dig deep to find the diamonds. I turn complex problems into beautiful solutions. I mine code so you don't have to.
-				</p>
-			</div>
+				<div class="container-fluid">
+					<h2>About Me</h2>
+					<h3>I'm a webdeveloping nurse</h3>
+					<p id="maincontent">
+						<link href='https://fonts.googleapis.com/css?family=VT323' rel='stylesheet' type='text/css'>
+						You have to dig deep to find the diamonds. I turn complex problems into beautiful solutions. I mine code so you don't have to.
+					</p>
+
+					<!--<div class="twitter-window">
+						<a class="twitter-timeline" href="https://twitter.com/JudeicusMaximus" data-widget-id="705497413117587456">Tweets by @JudeicusMaximus</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+					</div> -->
+
+					<form action="../php-libs/phpMailer.php" method="post" enctype="multipart/form-data">
+						<link href='https://fonts.googleapis.com/css?family=VT323' rel='stylesheet' type='text/css'>
+						<div class="form-group">
+							<label for="name">Your name:</label>
+							<input type="text" name="name"  id="name" required>
+						</div>
+						<div class="form-group">
+							<label for="email">Your email:</label>
+							<input type="email" name="email"  id="email" required>
+						</div>
+						<div class="form-group">
+							<label for="Subject">Subject:</label>
+							<input type="text" name="subject"  id="subject" required>
+						</div>
+						<div class="form-group">
+							<label for="message">Your message:</label>
+							<textarea name="message" id="message" required> </textarea>
+						</div>
+						<div class="form-group">
+							<input type="submit" value="Send Message!">
+						</div>
+					</form>
+
+				</div>
+			</div> <!--dirt-->
 		</div><!--/.sfooter-content-->
 
 		<footer id="rock">
 			<nav class="navbar">
 				<div class="container-fluid">
-					<div class="navbar-header">
+					<!--<div class="navbar-header">
 						<a class="navbar-brand" href="#">Check Me Out!</a>
-					</div>
-					<ul class="nav navbar-nav">
-						<li><a href="https://twitter.com/JudeicusMaximus">Twitter</a></li>
-						<li><a href="https://www.facebook.com/judeicusMaximus">Facebook</a></li>
-						<li><a href="https://www.linkedin.com/in/jude-chavez-87a519114">Linked In</a></li>
-						<li><a href="https://soundcloud.com/judeicusmaximus">Soundcloud</a></li>
-						<li><a href="https://www.youtube.com/channel/UCBuoSq2gvaHA5IYaL2FVZMA">YouTube</a></li>
+					</div>-->
+					<ul class="nav navbar-nav social-icons">
+						<li><a href="https://twitter.com/JudeicusMaximus"><i class="fa fa-twitter-square twitter"></i></a></li>
+						<li><a href="https://www.facebook.com/judeicusMaximus"><i class="fa fa-facebook-square facebook"></i></a></li>
+						<li><a href="https://www.linkedin.com/in/jude-chavez-87a519114"><i class="fa fa-linkedin-square linkedin"></i></a></li>
+						<li><a href="https://soundcloud.com/judeicusmaximus"><i class="fa fa-soundcloud soundcloud"></i></a></li>
+						<li><a href="https://www.youtube.com/channel/UCBuoSq2gvaHA5IYaL2FVZMA"><i class="fa fa-youtube-play youtube"></i></a></li>
+						<li><a href="https://www.instagram.com/judeicusmaximus/"><i class="fa fa-instagram instagram"></i></a></li>
+						<li><a href="https://github.com/chavezjude7"><i class="fa fa-github-square github"></i></a></li>
+						<li><a href="http://judeicusmaximus.tumblr.com"><i class="fa fa-tumblr-square tumblr"></i></a></li>
+						<li><a href="https://plus.google.com/u/0/115568638547980407676/posts"><i class="fa fa-google-plus-square google"></i></a></li>
 					</ul>
 				</div>
 			</nav>
